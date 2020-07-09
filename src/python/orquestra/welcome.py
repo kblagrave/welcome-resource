26ec07764fcb7f4bb1552c02ca92429972b450fc
kblagrave <kevin.blagrave@gmail.com>
2020-07-09 13:39:04 -0400

    Copy and paste version

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -1,12 +1,15 @@
 """
-This module saves a welcome message
+This module saves a welcome message.
 """
 
 import json
 
 def welcome():
-    message = "Welcome to Orquestra."
-    message_dict = {'message': message,'schema':'message'}
-    # save output to file so it can be passed on or accessible at end
-    with open('welcome.json,'w') as f:
-        f.write(json.dumps(message_dict, indent=2))
+    message = "Welcome to Orquestra!"
+
+    message_dict = {}
+    message_dict["message"] = message
+    message_dict["schema"] = "message"
+
+    with open("welcome.json",'w') as f:
+        f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
